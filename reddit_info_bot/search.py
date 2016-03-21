90ad8eb595521b7321e2adf1ae278f0363acede2
nyov <nyov@nexnode.net>
2016-03-21 14:22:45 +0000

    remove (broken) search code

--- reddit_info_bot/search.py
+++ reddit_info_bot/search.py
@@ -2,16 +2,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 import logging
-import six.moves.http_cookiejar as cookielib
-try:
-    import BeautifulSoup as bs
-except ImportError:
-    import bs4 as bs
-import requests
-import re
-from parsel import Selector
-from six.moves.urllib.request import build_opener, HTTPCookieProcessor, urlopen
-
 import string
 from collections import OrderedDict
 from six.moves.urllib.parse import urlsplit, urlunsplit
@@ -19,122 +9,6 @@ from six.moves.urllib.parse import urlsplit, urlunsplit
 from .util import domain_suffix, remove_control_characters
 
 logger = logging.getLogger(__name__)
-
-
-def get_google_results(image_url, settings, limit=15): #limit is the max number of results to grab (not the max to display)
-    headers = {}
-    headers['User-Agent'] = settings['SEARCH_USER_AGENT']
-    response_text = requests.get('https://www.google.com/searchbyimage?image_url={0}'.format(image_url), headers=headers).content
-    response_text += requests.get('https://www.google.com/searchbyimage?image_url={0}&start=10'.format(image_url), headers=headers).content
-    #response_text = response_text[response_text.find('Pages that include'):]
-    tree = bs.BeautifulSoup(response_text)
-    list_class_results = tree.findAll(attrs={'class':'r'})
-    if len(list_class_results) == 0:
-        raise IndexError('No results')
-    if limit >= len(list_class_results):
-        limit = len(list_class_results)
-    results = [(list_class_results[i].find('a')['href'],re.sub('<.*?>', '', re.sub('&#\d\d;', "'", ''.join([str(j) for j in list_class_results[i].find('a').contents])))) for i in xrange(limit)]
-    return results
-
-def get_bing_results(image_url, settings, limit=15):
-    cj = cookielib.MozillaCookieJar('cookies.txt')
-    cj.load()
-    opener = build_opener(HTTPCookieProcessor(cj))
-    opener.addheaders = [('User-Agent', settings['SEARCH_USER_AGENT'])]
-    response_text = opener.open("https://www.bing.com/images/searchbyimage?FORM=IRSBIQ&cbir=sbi&imgurl="+image_url).read()
-    tree = bs.BeautifulSoup(response_text)
-    list_class_results = tree.findAll(attrs={'class':'sbi_sp'})
-    if len(list_class_results) == 0:
-        raise IndexError('No results')
-    if limit >= len(list_class_results):
-        limit = len(list_class_results)
-    results = [(list_class_results[i].findAll(attrs={'class':'info'})[0].find('a')['href'],list_class_results[i].findAll(attrs={'class':'info'})[0].find('a').contents[0]) for i in xrange(limit)]
-    return results
-
-def get_yandex_results(image_url, settings, limit=15):
-    headers = {}
-    headers['User-Agent'] = settings['SEARCH_USER_AGENT']
-    response_text = requests.get("https://www.yandex.com/images/search?img_url={0}&rpt=imageview&uinfo=sw-1440-sh-900-ww-1440-wh-775-pd-1-wp-16x10_1440x900".format(image_url), headers=headers).content
-    response_text = response_text[response_text.find("Sites where the image is displayed"):]
-    tree = bs.BeautifulSoup(response_text)
-    list_class_results = tree.findAll(attrs={'class':'link other-sites__title-link i-bem'})
-    if len(list_class_results) == 0:
-        raise IndexError('No results')
-    results = []
-    for a in list_class_results:
-        a = str(a)
-        b = "https:"+a[a.find('href="')+6:a.find('" target="')]
-        filtered_link = re.compile(r'\b(amp;)\b', flags=re.IGNORECASE).sub("",b)
-        try:
-            redirect_url = urlopen(filtered_link).geturl()
-            text = a[a.find('"_blank">')+9:a.find('</a>')]
-            results.append((redirect_url,text))
-        except: pass #this site bands bots and cannot be accessed
-    if limit >= len(results):
-        limit = len(results)
-    return results[:limit]
-
-def get_karmadecay_results(image_url, settings, limit=15):
-    headers = {}
-    headers['User-Agent'] = settings['SEARCH_USER_AGENT']
-    response_text = requests.get("http://karmadecay.com/search?kdtoolver=b1&q="+image_url, headers=headers).content
-    if "No very similar images were found." in response_text:
-        return []
-    raw_results_text = response_text[response_text.find(":--|:--|:--|:--|:--")+20:response_text.find("*[Source: karmadecay]")-2]
-    raw_results = raw_results_text.split("\n")
-    results = [(i[i.find("(",i.find(']'))+1:i.find(")",i.find(']'))],i[i.find("[")+1:i.find("]")]) for i in raw_results]
-    return results #[(link,text)]
-
-def get_tineye_results(image_url, settings, limit=15):
-    def extract(r, count):
-        sel = Selector(text=r.content.decode(r.encoding))
-        page = sel.xpath('//div[@class="results"]//div[@class="row matches"]//div[contains(@class, "match-row")]')
-        if not page:
-            raise IndexError('No search results')
-        if 'Your IP has been blocked' in page:
-            logger.error('Tineye IP ban detected')
-            raise IndexError('No search results')
-        if '403 Forbidden' in page: # hmm, something else?
-            raise IndexError('No search results')
-
-        results = []
-        for found in page:
-            count -= 1
-            if count < 0:
-                break # stop after our search limit
-            source_image = found.xpath('.//div[@class="match"]/p[contains(@class, "short-image-link")]/a/@href').extract_first()
-            source_image_size = found.xpath('.//div[contains(@class, "match-thumb")]/p/span[2]/text()').extract_first()
-            source_link = found.xpath('.//div[@class="match"]/p[not(@class)]/a/@href').extract_first()
-            source_title = found.xpath('.//div[@class="match"]/h4[@title]/text()').extract_first()
-            #source_text = found.xpath('.//div[@class="match"]/p[@class="crawl-date"]/text()').extract_first()
-
-            if source_image:
-                source_image = os.path.basename(source_image)
-                text = '{0} {1} on {2}'.format(source_image, source_image_size, source_title)
-                results += [(source_link, text)]
-        return results # [(link,text)]
-
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'DNT': '1',
-        'Host': 'www.tineye.com',
-        'Referer': 'https://www.tineye.com/',
-    }
-    headers['User-Agent'] = settings['SEARCH_USER_AGENT']
-    response = requests.post("https://www.tineye.com/search", data={'url': image_url})
-
-    results = extract(response, limit)
-    limit = limit - len(results)
-    if limit > 0: # try another page
-        sel = Selector(text=response.content.decode(response.encoding))
-        next_link = sel.xpath('//div[@class="pagination"]/span[@class="current"]/following-sibling::a/@href').extract_first()
-        if next_link:
-            response = requests.get(response.url + '?page=2')
-            results += extract(response, limit)
-
-    return results
 
 
 def optimize_image_url(image_url):
@@ -167,11 +41,11 @@ def image_search(settings, image_url):
     image_url = optimize_image_url(image_url)
 
     search_engines = OrderedDict([
-        ('Google', get_google_results),
-        ('Bing',   get_bing_results),
-        ('Yandex', get_yandex_results),
-        ('Tineye', get_tineye_results),
-        ('Karma Decay', get_karmadecay_results),
+        ('Google', None),
+        ('Bing',   None),
+        ('Yandex', None),
+        ('Tineye', None),
+        ('Karma Decay', None),
     ])
 
     results = OrderedDict()
