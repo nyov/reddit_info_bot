c0248d7595e012459d8e7b8aee7e40087c855290
nyov <nyov@nexnode.net>
2016-05-23 18:21:55 +0000

    backport subprocess.check_output

--- reddit_info_bot/__init__.py
+++ reddit_info_bot/__init__.py
@@ -3,5 +3,9 @@ reddit_info_bot
 """
 from __future__ import absolute_import, unicode_literals
 
+# apply monkey patches for scrapy
+from . import _monkeypatches
+del _monkeypatches
+
 from .version import __version__, version_info
 from .cli import execute
